9aed8bd4604b92051cc2c8a805bde3369295df98
Mahesh-Borse <87294241+Mahesh-Borse@users.noreply.github.com>
2021-07-18 17:40:25 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,42 @@
+/*=== Night Mode/Dark Mode ====*/
+nightMode = () => {
+    document.documentElement.style.setProperty('--font-color', '#fff');
+    document.documentElement.style.setProperty('--background-color', '#000');
+    document.documentElement.style.setProperty('--footer-color', 'rgb(197, 197, 197)');
+    document.getElementById('light_dark_mode').innerHTML = " <i class='fa fa-moon'></i> ";
+}
+lightMode = () => {
+    document.documentElement.style.setProperty('--font-color', '#000');
+    document.documentElement.style.setProperty('--background-color', '#fff');
+    document.documentElement.style.setProperty('--footer-color', 'rgb(34, 34, 34)');
+    document.getElementById('light_dark_mode2').innerHTML = " <i class='fa fa-sun'></i> ";
+}
+color_Mode = () => {
+    document.documentElement.style.setProperty('--primary-color', '#d240ff');
+}
+color_Mode2 = () => {
+    document.documentElement.style.setProperty('--primary-color', '#ff00b3');
+}
+color_Mode3 = () => {
+    document.documentElement.style.setProperty('--primary-color', '#ff2d2d');
+}
+color_Mode4 = () => {
+    document.documentElement.style.setProperty('--primary-color', '#ff6600');
+}
+color_Mode5 = () => {
+    document.documentElement.style.setProperty('--primary-color', '#e4d500');
+}
+color_Mode6 = () => {
+    document.documentElement.style.setProperty('--primary-color', '#00ca0a');
+}
+color_Mode7 = () => {
+    document.documentElement.style.setProperty('--primary-color', '#00ddc0');
+}
+color_Mode8 = () => {
+    document.documentElement.style.setProperty('--primary-color', '#75a1d5');
+}
+
+
 /*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
@@ -65,9 +104,15 @@ myModal.addEventListener('shown.bs.modal', function () {
     myInput.focus();
 });
 
-/*=== Night Mode/Dark Mode ====*/
 
-// mode = () => {
-//     document.getElementById('light_dark_mode').innerHTML = " <i class='fa fa-sun'></i>&nbsp;Mode ";
-// }
+function showThemes() {
+    var checkboxes = document.getElementById("checkBoxes");
 
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+    }
+}
